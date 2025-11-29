f63bb1e8feeb87599689b8083de2795621d88933
vladimir <vladimir.v.glinskikh@gmail.com>
2025-11-29 21:38:35 +0300

    Setup main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { NestFactory } from '@nestjs/core'
+
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule)
+
+	const config = app.get(ConfigService)
+	const logger = new Logger(AppModule.name)
+
+	const port = config.getOrThrow<number>('HTTP_PORT')
+	const host = config.getOrThrow<string>('HTTP_HOST')
+	try {
+		await app.listen(port)
+		logger.log(`Server is running at: ${host}`)
+	} catch (error) {
+		logger.error(`Failed to start server: ${error.message}`, error)
+		process.exit(1)
+	}
 }
-bootstrap();
+bootstrap()
