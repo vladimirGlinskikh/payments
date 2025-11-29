6be73181f0ec7d746eb589ce5f6c2715aba2572d
vladimir <vladimir.v.glinskikh@gmail.com>
2025-11-29 21:53:10 +0300

    Create CORS

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,15 @@ import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
+import { getCorsConfig } from './config'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 
 	const config = app.get(ConfigService)
 	const logger = new Logger(AppModule.name)
+
+	app.enableCors(getCorsConfig(config))
 
 	const port = config.getOrThrow<number>('HTTP_PORT')
 	const host = config.getOrThrow<string>('HTTP_HOST')
