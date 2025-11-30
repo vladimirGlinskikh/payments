a4826daec04d2ed298f00fdab8c6ca8c96bdfe7f
vladimir <vladimir.v.glinskikh@gmail.com>
2025-11-30 12:53:36 +0300

    Added prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { InfraModule } from './infra/infra.module'
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
-			isGlobal: true
-		})
+			isGlobal: true,
+			envFilePath: '.env'
+		}),
+		InfraModule
 	]
 })
 export class AppModule {}
