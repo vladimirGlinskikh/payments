0cf541c23a0a0f9103f5023b849b9d44b21b125e
vladimir <vladimir.v.glinskikh@gmail.com>
2025-12-06 21:12:21 +0300

    Write RefreshTokenExpires

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { JwtService } from '@nestjs/jwt'
 import { User } from '@prisma/client'
 import { hash } from 'argon2'
 
+import { ms, StringValue } from '../../common/utils'
 import { PrismaService } from '../../infra/prisma/prisma.service'
 
 import { RegisterDto } from './dto'
@@ -11,18 +12,18 @@ import { JwtPayload } from './interfaces'
 
 @Injectable()
 export class AuthService {
-	private readonly JWT_ACCESS_TOKEN_TTL: string
-	private readonly JWT_REFRESH_TOKEN_TTL: string
+	private readonly JWT_ACCESS_TOKEN_TTL: StringValue
+	private readonly JWT_REFRESH_TOKEN_TTL: StringValue
 
 	public constructor(
-		private readonly prismaservice: PrismaService,
+		private readonly prismaService: PrismaService,
 		private readonly configService: ConfigService,
 		private readonly jwtService: JwtService
 	) {
-		this.JWT_ACCESS_TOKEN_TTL = this.configService.getOrThrow<string>(
+		this.JWT_ACCESS_TOKEN_TTL = this.configService.getOrThrow<StringValue>(
 			'JWT_ACCESS_TOKEN_TTL'
 		)
-		this.JWT_REFRESH_TOKEN_TTL = this.configService.getOrThrow<string>(
+		this.JWT_REFRESH_TOKEN_TTL = this.configService.getOrThrow<StringValue>(
 			'JWT_REFRESH_TOKEN_TTL'
 		)
 	}
@@ -30,14 +31,14 @@ export class AuthService {
 	public async register(dto: RegisterDto) {
 		const { name, email, password } = dto
 
-		const exists = await this.prismaservice.user.findUnique({
+		const exists = await this.prismaService.user.findUnique({
 			where: {
 				email
 			}
 		})
 		if (exists) throw new ConflictException('User is already registered')
 		const hashedPassword = await hash(password)
-		const user = await this.prismaservice.user.create({
+		const user = await this.prismaService.user.create({
 			data: {
 				name,
 				email,
@@ -50,6 +51,10 @@ export class AuthService {
 	private async generateTokens(user: User) {
 		const payload: JwtPayload = { id: user.id }
 
+		const refreshTokenExpires = new Date(
+			Date.now() + ms(this.JWT_REFRESH_TOKEN_TTL)
+		)
+
 		const accessToken = await this.jwtService.signAsync(payload, {
 			expiresIn: this.JWT_ACCESS_TOKEN_TTL,
 			secret: this.configService.getOrThrow<string>('JWT_SECRET')
@@ -60,6 +65,6 @@ export class AuthService {
 			secret: this.configService.getOrThrow<string>('REFRESH_SECRET')
 		} as any)
 
-		return { accessToken, refreshToken }
+		return { accessToken, refreshToken, refreshTokenExpires }
 	}
 }
