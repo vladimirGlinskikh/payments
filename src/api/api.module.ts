bbe0ef991ce7e0c1ece0d568281570313a51f56c
vladimir <vladimir.v.glinskikh@gmail.com>
2025-12-14 17:56:15 +0300

    Added entity for payments

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -3,8 +3,9 @@ import { Module } from '@nestjs/common'
 import { AuthModule } from './auth/auth.module'
 import { PlanModule } from './plan/plan.module'
 import { UsersModule } from './users/users.module'
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-	imports: [AuthModule, UsersModule, PlanModule]
+	imports: [AuthModule, UsersModule, PlanModule, PaymentModule]
 })
 export class ApiModule {}
